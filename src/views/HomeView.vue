ece0693b404192a44c9d03dea33850395863b7c0
sebaszap123-dev <sebaszap123@gmail.com>
2024-04-26 00:34:21 -0600

    first step portfolio

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="w-full h-lvh flex lg:items-center lg:justify-center lg:flex-wrap md:row-auto mx-auto">
+    <div class="md:flex lg:w-1/2 lg:h-full lg:items-center lg:justify-center py-10">
+      <p class="">Muchas cosas</p>
+    </div>
+    <div class="md:flex lg:w-1/2 lg:h-full lg:items-center lg:justify-center py-10">
+      <p class="">Muchas cosas</p>
+    </div>
+  </div>
+  <div class="w-full h-lvh bg-cyan-800"></div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped lang="css"></style>
